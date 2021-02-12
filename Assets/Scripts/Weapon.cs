067e8eacf0f426f77a88eee305b302446496efc1
Daroxies <78978836+Daroxies@users.noreply.github.com>
2021-02-12 20:04:59 +0100

    Initial commit

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask whatToHit;
+
+    public Transform BulletTrailPrefab;
+    public Transform MuzzleFlashPrefab;
+    float timeToSpwanEffect = 0;
+    public float effectSpawnRate = 10;
+
+    float timeToFire = 0;
+    Transform firePoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = transform.FindChild("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No FirePoint");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else { 
+            if(Input.GetButton ("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        //Stores the mouseposition in pixels not coordinates
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        //Stores position of the Firepoint (the point in front of the gun)
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+        if (Time.time >=timeToSpwanEffect)
+        {
+            Effect();
+            timeToSpwanEffect = Time.time + 1 / effectSpawnRate;
+        }
+        
+        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
+        //if hit something
+        if(hit.collider !=null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + "and did " + Damage + "damage.");
+        }
+    }
+
+    void Effect()
+    {   //clone
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        //clone and save as Transform
+        Transform clone = Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+        clone.parent = firePoint;
+        float size = Random.Range(0.6f, 0.9f);
+        clone.localScale = new Vector3(size, size, size);
+        Destroy(clone.gameObject, 0.02f);
+    }
+
+}
+
